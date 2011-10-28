52e0d19092a764eb3a83e97594ee3423e7a4b035
John <jsstark@gmail.com>
2011-10-28 22:41:50 +0300

    a simple test for RVObjectiveFunctionCreate/Destroy

--- simple-client-c/main.c
+++ simple-client-c/main.c
@@ -1,9 +1,17 @@
 #include "revolution.h"
 #include <stdio.h>
 
+int evalf(const double *dv, double *obj)
+{
+	return 0;
+}
+
 int main(int argc, char *argv[])
 {
 	printf("API: %d.%d.%d\n", RVGetMajorVersion(), RVGetMinorVersion(), RVGetPatchVersion());
 	printf("API: %d.%d.%d\n", RV_API_MAJOR, RV_API_MINOR, RV_API_PATCH);
+
+	RVObjectiveFunction *object = RVObjectiveFunctionCreate(3, 2, evalf);
+	RVObjectiveFunctionDestroy(object);
 	return 0;
 }
--- simple-client/main.cpp
+++ simple-client/main.cpp
@@ -1,6 +1,11 @@
 #include "revolution.h"
 #include <iostream>
 
+int evalf(const double *dv, double *obj)
+{
+	return 0;
+}
+
 int main(int argc, char *argv[])
 {
 	std::cout << "API: " 
@@ -10,6 +15,9 @@ int main(int argc, char *argv[])
 
 	std::cout << "API: "
 			<< RV_API_MAJOR << "." << RV_API_MINOR << "." << RV_API_PATCH << std::endl;
+
+	RVObjectiveFunction *object = RVObjectiveFunctionCreate(3, 2, evalf);
+	RVObjectiveFunctionDestroy(object);
 
 	return 0;
 }
