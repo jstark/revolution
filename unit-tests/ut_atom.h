2aa3769933abe288d91a8d43f51de33fb78a456a
John <jsstark@gmail.com>
2011-12-15 23:35:38 +0200

    unit tests for Atom

---
+++ unit-tests/ut_atom.h
@@ -0,0 +1,87 @@
+#include "gtest/gtest.h"
+#include "atom.h"
+#include "objective_function.h"
+#include <limits>
+
+namespace 
+{
+    const double MAX_F_VAL = std::numeric_limits<double>::max();
+
+    TEST (AtomTest, atomInitializesCorrectly)
+    {
+        revolution::Atom a(5,2);
+        ASSERT_EQ(5, a.dim()) << "Dim error!";
+        ASSERT_EQ(2, a.obj()) << "Obj error!";
+
+        for (int i = 0; i < 5; ++i)
+            EXPECT_DOUBLE_EQ(a[i], 0.0)  << "a[" << i << "] != 0.0";
+
+        EXPECT_DOUBLE_EQ(a.f(0), MAX_F_VAL) << "a.f(0) != " << MAX_F_VAL;
+        EXPECT_DOUBLE_EQ(a.f(1), MAX_F_VAL) << "a.f(1) != " << MAX_F_VAL;
+    }
+
+    TEST (AtomTest, setDesignVariables)
+    {
+        revolution::Atom a(5,2);
+
+        for (int i = 0; i < 5; ++i)
+        {
+            a[i] = i;
+            EXPECT_DOUBLE_EQ(a[i], i);
+        }
+    }
+
+    class AtomFixture : public ::testing::Test 
+    {
+        protected:
+            AtomFixture() 
+            {
+                sphere3 = revolution::ObjectiveFunction::create(3, 1, sphere3_eval, 0);
+            }
+
+            virtual ~AtomFixture()
+            {
+                delete sphere3;
+            }  
+
+            revolution::ObjectiveFunction *sphere3;
+
+            static int sphere3_eval(const double *params, double *obj, void *data)
+            {
+                double x = params[0], y = params[1], z = params[2];
+                obj[0] = x*x + y*y + z*z;
+                return 0;
+            }
+
+            static void init(double *params, double *obj, void *data)
+            {
+                params[0] = 1.0;
+                params[1] = 2.0;
+                params[2] = 3.0;
+            }
+    };
+
+    TEST_F (AtomFixture, checkObjectiveValues)
+    {
+        revolution::Atom a(3,1);
+
+        a[0] = 1;
+        a[1] = 2;
+        a[2] = 3;
+
+        a.eval(*sphere3);
+        EXPECT_DOUBLE_EQ(a.f(0), 14.0) << "a.f(0) != 14";
+    }
+
+    TEST_F (AtomFixture, checkInitialization)
+    {
+        revolution::Atom a(3,1);
+
+        a.initialize(init, 0);
+
+        EXPECT_DOUBLE_EQ(a[0], 1.0) << "a[0] not initialized correctly!";
+        EXPECT_DOUBLE_EQ(a[1], 2.0) << "a[1] not initialized correctly!";
+        EXPECT_DOUBLE_EQ(a[2], 3.0) << "a[2] not initialized correctly!";
+    }
+
+}//~ namespace
---
+++ unit-tests/ut_population.h
@@ -0,0 +1,15 @@
+#include "gtest/gtest.h"
+#include "population.h"
+
+namespace 
+{
+
+    class UT_Population : public ::testing::Test 
+    {
+
+    };
+
+    TEST_F (UT_Population, populationInitializesCorrectly)
+    {
+    }
+}//~ namespace
