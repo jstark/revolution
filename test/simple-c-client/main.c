ed820be08e4a6158e0d5848905f08478bb9f5465
John <jsstark@gmail.com>
2012-01-29 19:59:45 +0200

    Sphere2 problem solved with DE

--- test/simple-c-client/main.c
+++ test/simple-c-client/main.c
@@ -1,22 +1,31 @@
 #include "revolution.h"
 #include <stdio.h>
 
-int evalf(const struct RVArray *dv, struct RVArray *obj, void *data)
+int sphere2(const struct RVArray *dv, struct RVArray *obj, void *data)
 {
+	double x = RVArrayGetElementAtIndex(dv, 0);
+	double y = RVArrayGetElementAtIndex(dv, 1);
+	RVArraySetElementAtIndex(obj, 0, x*x + y*y);
 	return 0;
 }
 
-int main(int argc, char *argv[])
+void initPopulation(struct RVArray *dv, struct RVArray *obj, void *data)
 {
-	struct RVObjectiveFunction *object = RVObjectiveFunctionCreate(3, 2, evalf, NULL);
-	struct RVBasicEvolutionStrategy *es = RVBasicEvolutionStrategyCreate(3, 1, 4, kRVSelectionModeComma, object);
-	RVBasicEvolutionStrategyDestroy(es);
-    struct RVCmaEvolutionStrategy *cma_es = RVCmaEvolutionStrategyCreate(10, object);
-    RVCmaEvolutionStrategyDestroy(cma_es);
-	
-    
-    struct RVDifferentialEvolution *de = RVDifferentialEvolutionCreate(10, 0.5, 0.5, object);
-    RVDifferentialEvolutionDestroy(de);
+	RVArraySetElementAtIndex(dv, 0, 10.0 * rand() / (double)RAND_MAX);
+	RVArraySetElementAtIndex(dv, 1, 10.0 * rand() / (double)RAND_MAX);
+}
+
+int main(int argc, char *argv[])
+{    
+	struct RVObjectiveFunction *object = RVObjectiveFunctionCreate(2, 1, sphere2, NULL);
+    struct RVDifferentialEvolution *de = RVDifferentialEvolutionCreate(10, 0.8, 0.2, object);
+	RVDifferentialEvolutionInitializePopulation(de, initPopulation, NULL);
+	RVDifferentialEvolutionStart(de);
+	printf("X = %f, Y = %f\tF: %f\n",
+		   RVDifferentialEvolutionGetDesignParameter(de, 0, 0),
+		   RVDifferentialEvolutionGetDesignParameter(de, 0, 1),
+		   RVDifferentialEvolutionGetObjective(de, 0, 0));
+	RVDifferentialEvolutionDestroy(de);
     RVObjectiveFunctionDestroy(object);
 	return 0;
 }
